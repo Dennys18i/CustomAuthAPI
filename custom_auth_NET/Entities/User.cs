80687b133207d91b14eacd3c33067dadaafdd28d
Denis Antoci <antoci136@gmail.com>
2025-11-02 22:04:37 +0200

    basic_implementation_for_register_login

---
+++ custom_auth_NET/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace custom_auth_NET.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
